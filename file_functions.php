967404d0359fe2fc200dd730d13e8cd1b86ee09c
Justin <jforseth210@gmail.com>
2021-01-05 20:06:35 -0700

    Remove uneeded code and move savePhoto function to file_functions.php

--- file_functions.php
+++ file_functions.php
@@ -3,76 +3,52 @@
 require_once("array_shove.php");
 
 
+function savePhoto($photo, $target_file)
+{
+    $uploadOk = 1;
 
-//This functions *should* be obsolete.
-//I plan to delete them.
-function deleteRow($rowToDelete, $filename){
-    //Read original file
-    $readFile = fopen("../data/" . $filename, "r");
-    $readText = fread($readFile, filesize("../data/" . $filename));
-    fclose($readFile);
-    //Split file by line
-    $readArray = explode("\n", $readText);
-    //Open the file for writing
-    $writeFile = fopen("../data/" . $filename, "w");
-    $writeString = "";
-    //Iterate through the rows, update the one the that the user modified.
-    for ($row = 0; $row <= sizeof($readArray) - 1; $row++) {
-        if ($row != $rowToDelete) {
-            $writeString = $writeString . $readArray[$row] . "\n";
-        }
+    // Check if image file is a actual image or fake image
+    $check = getimagesize($photo["tmp_name"]);
+    if ($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
     }
 
-    //If this line is uncommented, the final line
-    //becomes truncated intermittently.
-    //If it is commented, the file will grow in size
-    //on every update.
+    // Check file size
+    /*if ($photo["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+    }*/
 
-    //$fwstring = str_replace("\n\n", "\n", $fwstring);
+    // Allow certain file formats
+    $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
+    $imageFileType = strtolower($imageFileType);
+    if (
+        $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+        && $imageFileType != "gif"
+    ) {
+        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+        $uploadOk = 0;
+    }
 
-    //Write and close the file.
-    fwrite($writeFile, $writeString);
-    fclose($writeFile);
-}
-
-function updateRow($rowToUpdate, $newString, $filename){
-    //Read original file
-    $readFile = fopen("../data/" . $filename, "r");
-    $readText = fread($readFile, filesize("../data/" . $filename));
-    fclose($readFile);
-    //Split file by line
-    $readArray = explode("\n", $readText);
-    //Open the file for writing
-    $writeFile = fopen("../data/" . $filename, "w");
-    $writeString = "";
-    //Iterate through the rows, update the one the that the user modified.
-    for ($row = 0; $row <= sizeof($readArray) - 1; $row++) {
-        if ($row == $rowToUpdate) {
-            $writeString = $writeString . $newString . "\n";
+    // Check if $uploadOk is set to 0 by an error
+    if ($uploadOk == 0) {
+        echo "Sorry, your file was not uploaded.";
+        // if everything is ok, try to upload file
+    } else {
+        if (move_uploaded_file($photo["tmp_name"], $target_file)) {
+            echo "The file " . htmlspecialchars(basename($photo["name"])) . " has been uploaded.";
         } else {
-            $writeString = $writeString . $readArray[$row] . "\n";
+            echo "Sorry, there was an error uploading your file.";
         }
     }
-
-    //If this line is uncommented, the final line
-    //becomes truncated intermittently.
-    //If it is commented, the file will grow in size
-    //on every update.
-
-    $writeString = str_replace("\n\n", "\n", $writeString);
-
-    //Write and close the file.
-    fwrite($writeFile, $writeString);
-    fclose($writeFile);
 }
 
-function addNewRow($stringToAdd, $filename){
-    $appendFile = fopen("../data/" . $filename, "a");
-    fwrite($appendFile, $stringToAdd . "\n");
-    fclose($appendFile);
-}
-
-function deleteRowJSON($rowToDelete, $filename){
+function deleteRowJSON($rowToDelete, $filename)
+{
     $readArray = readArrayFromJSON($filename);
 
     unset($readArray[$rowToDelete]);
@@ -80,7 +56,8 @@ function deleteRowJSON($rowToDelete, $filename){
     writeArrayToJSON($readArray, $filename);
 }
 
-function updateRowJSON($rowToUpdate, $updatedArray, $filename){
+function updateRowJSON($rowToUpdate, $updatedArray, $filename)
+{
     $readArray = readArrayFromJSON($filename);
 
     $readArray[$rowToUpdate] = $updatedArray;
@@ -88,7 +65,8 @@ function updateRowJSON($rowToUpdate, $updatedArray, $filename){
     writeArrayToJSON($readArray, $filename);
 }
 
-function addNewRowJSON($arrayToAdd, $filename){
+function addNewRowJSON($arrayToAdd, $filename)
+{
     $readArray = readArrayFromJSON($filename);
 
     array_push($readArray, $arrayToAdd);
@@ -96,13 +74,15 @@ function addNewRowJSON($arrayToAdd, $filename){
     writeArrayToJSON($readArray, $filename);
 };
 
-function readArrayFromJSON($filename){
+function readArrayFromJSON($filename)
+{
     $fileContents = file_get_contents("../data/" . $filename);
     $readArray = json_decode($fileContents, true);
     return $readArray;
 };
 
-function writeArrayToJSON($writeArray, $filename){
+function writeArrayToJSON($writeArray, $filename)
+{
     $writeString = json_encode(array_values($writeArray), JSON_PRETTY_PRINT);
 
     $writeFile = fopen("../data/" . $filename, "w");
@@ -110,9 +90,9 @@ function writeArrayToJSON($writeArray, $filename){
     fclose($writeFile);
 }
 
-function reorderArrayJSON($filename, $rowToMove,$direction){
-  $originalArray = readArrayFromJSON($filename);
-  $newArray = array_shove($originalArray, $rowToMove, $direction);
-  writeArrayToJSON($newArray, $filename);
+function reorderArrayJSON($filename, $rowToMove, $direction)
+{
+    $originalArray = readArrayFromJSON($filename);
+    $newArray = array_shove($originalArray, $rowToMove, $direction);
+    writeArrayToJSON($newArray, $filename);
 }
-?>
