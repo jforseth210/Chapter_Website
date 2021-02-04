54e03dd199a0ef0f32c34e10a98906b6014f45d0
Justin <jforseth210@gmail.com>
2021-02-04 09:51:52 -0700

    Re-add image submission route?

--- admin_templates/about_us_image_gallery.php
+++ admin_templates/about_us_image_gallery.php
@@ -13,7 +13,21 @@ if (isset($_POST["aboutUsPhotoNewSubmit"])) {
   AddNewRowJSON($aboutUsPhotoArray, "aboutUsImageGallery.json");
   savePhoto($photo, $target_file);
 }
+if (isset($_POST["aboutUsPhotoSubmit"])) {
+  $photo = $_FILES['photo'];
+  $rowToUpdate = intVal($_POST['row_num']);
 
+  $imageFileType = strtolower(pathinfo($photo["name"], PATHINFO_EXTENSION));
+
+  //Save as images/aboutUsPhotos/nameOfOffice.fileExtension
+  $target_dir = "images/about_us_gallery/";
+  $target_file = $target_dir . basename($photo["name"]);
+  $aboutUsPhotoArray = array(
+    "path" => $target_file
+  );
+  updateRowJSON($rowToUpdate, $aboutUsPhotoArray, "aboutUsImageGallery.json");
+  savePhoto($photo, $target_file);
+}
 if (isset($_POST['aboutUsPhotoDeleteSubmit'])) {
   //Get the row number of the officer being modified
   $rowToDelete = intVal($_POST['row_num']);
@@ -53,29 +67,29 @@ if (isset($_POST['aboutUsPhotoCardsReorderSubmit'])) {
                     
                     <form role='form' id=\"aboutUsPhotos$aboutUsPhoto" . "imagechange" . "\" action='" . htmlspecialchars($_SERVER['PHP_SELF']) . "#aboutUsPhotos" . "' method=\"POST\" enctype=\"multipart/form-data\">
                         <input hidden name=row_num  value=\"$aboutUsPhoto\">
-                        <input name=\"photoSubmit\" type=\"submit\" value=\"Save\" class=\"col-4 submit-button btn btn-primary\" />
                         <div class=\"custom-file\">
-                            <input type=\"file\" name=\"photo\" class=\"new-load-file-function fresh-id form-control-file\" id=\"customAboutUsPhoto$aboutUsPhoto\" onchange=\"loadFile(event, 'aboutUsPhoto$aboutUsPhoto')\">
-                            <label class=\"fresh-for custom-file-label\" for=\"customAboutUsPhoto$aboutUsPhoto\">Choose file</label>
+                        <input type=\"file\" name=\"photo\" class=\"new-load-file-function fresh-id form-control-file\" id=\"customAboutUsPhoto$aboutUsPhoto\" onchange=\"loadFile(event, 'aboutUsPhoto$aboutUsPhoto')\">
+                        <label class=\"fresh-for custom-file-label\" for=\"customAboutUsPhoto$aboutUsPhoto\">Choose file</label>
                         </div>
-                    </form>
-                    
-                    <form role='form' id=\"" . "aboutUsPhoto" . $aboutUsPhoto . "Delete\" action='" . htmlspecialchars($_SERVER['PHP_SELF']) . "#aboutUsPhotos" . "' method=\"POST\">
+                        </form>
+                        
+                        <form role='form' id=\"" . "aboutUsPhoto" . $aboutUsPhoto . "Delete\" action='" . htmlspecialchars($_SERVER['PHP_SELF']) . "#aboutUsPhotos" . "' method=\"POST\">
                         <input hidden name=row_num value=\"$aboutUsPhoto\">
-                    </form>
-
-                    <input hidden name=row_num form=\"aboutUsPhotos$aboutUsPhoto\" value=\"$aboutUsPhoto\">
-                    
-                          
-                    <div class=\"mt-auto\">
+                        </form>
+                        
+                        <input hidden name=row_num form=\"aboutUsPhotos$aboutUsPhoto\" value=\"$aboutUsPhoto\">
+                        
+                        
+                        <div class=\"mt-auto\">
                         <div role=\"group\" class=\"btn-group mx-auto mt-auto w-100\">
-                            <input form=\"" . "aboutUsPhoto" . $aboutUsPhoto . "Delete\" 
-                            class=\"col-4 new-disable btn btn-danger mx-auto\" type=submit name=\"aboutUsPhotoDeleteSubmit\" value=\"Delete\" />
-                            
-                            
-                            <button type=\"button\" class=\"new-disable btn btn-success mx-auto\" onclick=\"newRow('aboutUsPhotoCards',$aboutUsPhoto);\">New</button>
+                        <input form=\"" . "aboutUsPhoto" . $aboutUsPhoto . "Delete\" 
+                        class=\"col-4 new-disable btn btn-danger mx-auto\" type=submit name=\"aboutUsPhotoDeleteSubmit\" value=\"Delete\" />
+                        
+                        <input form=\"aboutUsPhotos$aboutUsPhoto" . "imagechange" . "\" name=\"aboutUsPhotoSubmit\" type=\"submit\" value=\"Save\" class=\"col-4 submit-button btn btn-primary\" />
+                        
+                        <button type=\"button\" class=\"new-disable btn btn-success mx-auto\" onclick=\"newRow('aboutUsPhotoCards',$aboutUsPhoto);\">New</button>
                         </div>
-                    </div>
+                        </div>
                     
                 </div>
               </div>
