542ee6633c9db8f21fb0f142fee1c7330fd707df
Justin <jforseth210@gmail.com>
2021-02-05 14:31:32 -0700

    Fix logout

--- logout.php
+++ logout.php
@@ -1,10 +1,10 @@
-<?
+<?php
     /*
     Ends the session and
     redirects to the homepage
     */
     session_start();
     session_destroy();
-    header("chaptersitewip.php");
+    header("/chaptersitewip.php");
     exit();
 ?>
