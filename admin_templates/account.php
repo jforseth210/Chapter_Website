5710972fcada8e96327e3f5fc441676bfb6bb392
Justin <jforseth210@gmail.com>
2021-02-10 11:44:24 -0700

    Add password change section to admin

---
+++ admin_templates/account.php
@@ -0,0 +1,63 @@
+<?php
+  if (isset($_POST['change_password_submit'])) {
+    $old_password = $_POST['old_password'];  
+    $confirm_old_password = $_POST['confirm_old_password'];  
+    $new_password = $_POST['new_password'];  
+    echo ("old_password:" . $old_password);
+    echo ("confirm_old_password:" . $confirm_old_password);
+    echo ("new_password:" . $new_password);
+    if ($old_password == $confirm_old_password){
+      echo ("MATCH");
+      $users = readArrayFromJSON("users.json");
+      $user = array();
+      for($i = 0; $i < sizeof($users); $i++){
+        if ($users[$i]['username']==$_SESSION['username']){
+          echo ("USER FOUND");
+          $user = $users[$i];
+          if (password_verify($old_password, $user["password_hash"])){
+            echo ("PASSWORD VERIFIED");
+            echo ("OLD HASH:<br/>");
+            echo($user["password_hash"]);
+            $user["password_hash"] == password_hash($new_password, PASSWORD_DEFAULT);
+            echo ("NEW HASH:<br/>");
+            echo(password_hash($new_password, PASSWORD_DEFAULT));
+            $user["updated"]="True";
+            updateRowJSON($i, $user, "users.json");
+          } else {
+            echo("Old password is incorrect");
+          } 
+        } else {
+          echo("Passwords do not match!");
+        }
+      }
+    }
+  }
+?>
+<section id="accounts">
+  <div class="container">
+    <h2>Accounts</h2>
+        <?php
+        echo "<h5>Hello <b>{$_SESSION['real_name']}</b>. You are logged in as <b>{$_SESSION['username']}</b> and have <b>{$_SESSION['access']}</b> access.</b></b></h5><br /><br /><br /><br />";
+        ?>
+        <div class="row">
+        <div class="col-lg-6">
+        <?php if ($_SESSION["access"] != "admin") {
+                    echo "
+                    <h4>Change Password</h4>
+                    <form method=\"POST\" action='" . htmlspecialchars($_SERVER['PHP_SELF']) . "#aboutUsPhotos" . "' method=\"POST\" enctype=\"multipart/form-data\">
+                      <label>Old Password</label>
+                      <input class=\"form-control\" type=\"password\" name=\"old_password\"/>
+                      <label>Confirm Old Password</label>
+                      <input class=\"form-control\" type=\"password\" name=\"confirm_old_password\"/>
+                      <label>New Password</label>
+                      <input class=\"form-control\" type=\"password\" name=\"new_password\"/>
+                      <br />
+                      <input class=\"form-control btn btn-primary\" type=\"submit\" name=\"change_password_submit\" value=\"Submit\">
+                    </form>
+                    ";
+                }
+                ?>
+            </div>
+        </div>
+    </div>
+</section>
