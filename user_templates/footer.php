f48df3362bb00ef7a7abf5a3389efac64eb97ef6
Justin <jforseth210@gmail.com>
2021-02-16 17:05:59 -0700

    A little tidying on footer.php

--- user_templates/footer.php
+++ user_templates/footer.php
@@ -1,19 +1,20 @@
-  <!-- Footer -->
-  <footer class="py-5 bg-ffablue">
-    <div class="container">
-      <p class="m-0 text-center text-white">Copyright &copy; Fairfield FFA 2020.</p>
-      <p class="m-0 text-center text-white">Template by startbootstrap.com.</p>
-      <p class="m-0 text-center text-white">Designed by Justin Forseth.</p>
-      <p class="m-0 text-center text-white"><a href="admin.php">Login</a> to access admin page.</p>
-    </div>
-    <!-- /.container -->
-  </footer>
-    <!-- START Bootstrap-Cookie-Alert -->
-    <div class="alert bg-ffablue text-center cookiealert" role="alert">
-        This site uses cookies. By continuing to use this site, we assume you're okay with that. For enhanced privacy, you are encouraged to block third-party cookies. <a href="https://cookiesandyou.com/" target="_blank">Learn More</a>
+<!-- Footer -->
+<footer class="py-4 bg-ffablue">
+  <div class="container">
+    <p class="m-0 text-center text-white">Copyright &copy; Fairfield FFA 2020.</p>
+    <p class="m-0 text-center text-white">Template by startbootstrap.com.</p>
+    <p class="m-0 text-center text-white">Designed by Justin Forseth.</p>
+    <p class="m-0 text-center text-white"><a href="admin.php">Login</a> to access admin page.</p>
+  </div>
+  <!-- /.container -->
+</footer>
 
-        <button type="button" class="btn btn-primary btn-sm acceptcookies">
-            I agree
-        </button>
-    </div>
-    <!-- END Bootstrap-Cookie-Alert -->
+<!-- START Bootstrap-Cookie-Alert -->
+<div class="alert bg-ffablue text-center cookiealert" role="alert">
+  This site uses cookies. By continuing to use this site, we assume you're okay with that. For enhanced privacy, you are encouraged to block third-party cookies. <a href="https://cookiesandyou.com/" target="_blank">Learn More</a>
+
+  <button type="button" class="btn btn-primary btn-sm acceptcookies">
+    I agree
+  </button>
+</div>
+<!-- END Bootstrap-Cookie-Alert -->
