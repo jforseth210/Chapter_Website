51a7717e4cffe4fd2291a7e29e95b0c842e22c88
Justin <jforseth210@gmail.com>
2021-02-17 12:02:18 -0700

    Cleanup contact.php (admin)

--- admin_templates/contact.php
+++ admin_templates/contact.php
@@ -20,9 +20,9 @@ if (isset($_POST['contactInfoNewSubmit'])) {
     $contactInfo = str_replace("https://", "", $contactInfo);
 
     $newContactArray = array(
-        "contact_name"=>$contactName,
-        "contact_type"=>$contactType,
-        "contact_info"=>$contactInfo
+        "contact_name" => $contactName,
+        "contact_type" => $contactType,
+        "contact_info" => $contactInfo
     );
     addNewRowJSON($newContactArray, "contactInfoText.json");
 }
@@ -43,9 +43,9 @@ if (isset($_POST['contactInfoUpdateSubmit'])) {
     $contactInfo = str_replace("https://", "", $contactInfo);
 
     $newContactArray = array(
-        "contact_name"=>$contactName,
-        "contact_type"=>$contactType,
-        "contact_info"=>$contactInfo
+        "contact_name" => $contactName,
+        "contact_type" => $contactType,
+        "contact_info" => $contactInfo
     );
 
     updateRowJSON($rowToUpdate, $newContactArray, "contactInfoText.json");
@@ -93,51 +93,52 @@ if (isset($_POST['contactTableReorderSubmit'])) {
                             $optionsArray = array_keys($contactTypeConversion);
 
                             //Create the start of the row, which is also a form.
-                            echo "
+                        ?>
                             <tr>
-                            <form role='form' id=\"contact$contact\" action='" . htmlspecialchars($_SERVER['PHP_SELF']) . "#contact" . "' method=\"POST\">
-                                <td>
-                                    <input form=\"contact$contact\" name=\"contactName\" class=\"erasable-value form-control\" value=\"{$currentContactArray['contact_name']}\" />
-                                </td>
-                                <td>
-                                    <select form=\"contact$contact\" class=\"form-control erasable-value\"name=\"contactType\">
-                                ";
-                            for ($option = 0; $option <= sizeof($optionsArray) - 1; $option++) {
-                                if ($contactTypeConversion[$optionsArray[$option]] == $currentContactArray["contact_type"]) {
-                                    echo "<option selected>{$optionsArray[$option]}</option>";
-                                } else {
-                                    echo "<option>{$optionsArray[$option]}</option>";
-                                };
-                            }
-                            echo "
-                                    </select>
-                                </td>
-                                <td>
-                                    <input name=\"contactInfo\" form=\"contact$contact\" class=\"erasable-value form-control\" value=\"{$currentContactArray["contact_info"]}\" />
-                                </td>
-                                <td>
-                                    <button type=\"button\" class=\"new-disable btn btn-success\" onclick=\"newRow('contactTable',$contact);\">+</button>
-                                    <form role='form' id=\"contact" . $contact . "Delete\" action='" . htmlspecialchars($_SERVER['PHP_SELF']) . "#contact" . "' method=\"POST\">
-                                    <input class=\"new-disable btn btn-danger\" type=submit name=\"contactInfoDeleteSubmit\" value=\"-\"/>
-                                    </form>
-                                </td>
-                                <td width=\"120px\">
-                                <div class=\"btn btn-secondary handle\">
-                                <svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-arrows-move\" viewBox=\"0 0 16 16\">
-                                    <path fill-rule=\"evenodd\" d=\"M7.646.146a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 1.707V5.5a.5.5 0 0 1-1 0V1.707L6.354 2.854a.5.5 0 1 1-.708-.708l2-2zM8 10a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 0 1 .708-.708L7.5 14.293V10.5A.5.5 0 0 1 8 10zM.146 8.354a.5.5 0 0 1 0-.708l2-2a.5.5 0 1 1 .708.708L1.707 7.5H5.5a.5.5 0 0 1 0 1H1.707l1.147 1.146a.5.5 0 0 1-.708.708l-2-2zM10 8a.5.5 0 0 1 .5-.5h3.793l-1.147-1.146a.5.5 0 0 1 .708-.708l2 2a.5.5 0 0 1 0 .708l-2 2a.5.5 0 0 1-.708-.708L14.293 8.5H10.5A.5.5 0 0 1 10 8z\"/>
-                                </svg>
-                                </div>
-                                </td>
-                                <input hidden name=row_num form=\"contact$contact\" value=\"$contact\">
-                                <input hidden name=row_num form=\"contact" . $contact . "Delete\" value=\"$contact\">
-                                <input hidden name=row_num form=\"contact" . $contact . "MoveUp\" value=\"$contact\">
-                                <input hidden name=row_num form=\"contact" . $contact . "MoveDown\" value=\"$contact\">
-                                <td>
-                                    <input form=\"contact$contact\" class=\"btn btn-primary submit-button\" type=submit name=\"contactInfoUpdateSubmit\" value=\"Save\"/>
-                                </td>
-                              </form>
-                            </tr>";
-                            };
+                                <form role="form" id="contact<?php echo $contact; ?>" action="<?php htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
+                                    <td>
+                                        <input form="contact<?php echo $contact; ?>" name="contactName" class="erasable-value form-control" value="<?php echo $currentContactArray['contact_name'] ?>" />
+                                    </td>
+                                    <td>
+                                        <select form="contact<?php echo $contact; ?>" class="form-control erasable-value" name="contactType">
+                                            <?php
+                                            for ($option = 0; $option <= sizeof($optionsArray) - 1; $option++) {
+                                                if ($contactTypeConversion[$optionsArray[$option]] == $currentContactArray["contact_type"]) {
+                                                    echo "<option selected>{$optionsArray[$option]}</option>";
+                                                } else {
+                                                    echo "<option>{$optionsArray[$option]}</option>";
+                                                };
+                                            }
+                                            ?>
+                                        </select>
+                                    </td>
+                                    <td>
+                                        <input name="contactInfo" form="contact<?php echo $contact; ?>" class="erasable-value form-control" value="<?php echo $currentContactArray["contact_info"] ?>" />
+                                    </td>
+                                    <td>
+                                        <button type="button" class="new-disable btn btn-success" onclick="newRow('contactTable',<?php echo $contact; ?>);">+</button>
+                                        <form role='form' id="contact<?php echo $contact; ?>Delete" action='<?php htmlspecialchars($_SERVER['PHP_SELF']);?>#contact" . "' method="POST">
+                                            <input class="new-disable btn btn-danger" type=submit name="contactInfoDeleteSubmit" value="-" />
+                                        </form>
+                                    </td>
+                                    <td width="120px">
+                                        <div class="btn btn-secondary handle">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrows-move" viewBox="0 0 16 16">
+                                                <path fill-rule="evenodd" d="M7.646.146a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 1.707V5.5a.5.5 0 0 1-1 0V1.707L6.354 2.854a.5.5 0 1 1-.708-.708l2-2zM8 10a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 0 1 .708-.708L7.5 14.293V10.5A.5.5 0 0 1 8 10zM.146 8.354a.5.5 0 0 1 0-.708l2-2a.5.5 0 1 1 .708.708L1.707 7.5H5.5a.5.5 0 0 1 0 1H1.707l1.147 1.146a.5.5 0 0 1-.708.708l-2-2zM10 8a.5.5 0 0 1 .5-.5h3.793l-1.147-1.146a.5.5 0 0 1 .708-.708l2 2a.5.5 0 0 1 0 .708l-2 2a.5.5 0 0 1-.708-.708L14.293 8.5H10.5A.5.5 0 0 1 10 8z" />
+                                            </svg>
+                                        </div>
+                                    </td>
+                                    <input hidden name=row_num form="contact<?php echo $contact; ?>" value="<?php echo $contact; ?>">
+                                    <input hidden name=row_num form="contact<?php echo $contact; ?>Delete" value="<?php echo $contact; ?>">
+                                    <input hidden name=row_num form="contact<?php echo $contact; ?>MoveUp" value="<?php echo $contact; ?>">
+                                    <input hidden name=row_num form="contact<?php echo $contact; ?>MoveDown" value="<?php echo $contact; ?>">
+                                    <td>
+                                        <input form="contact<?php echo $contact; ?>" class="btn btn-primary submit-button" type=submit name="contactInfoUpdateSubmit" value="Save" />
+                                    </td>
+                                </form>
+                            </tr>
+                        <?php
+                        };
                         ?>
                     </tbody>
                 </table>
