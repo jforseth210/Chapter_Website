103b388136fe16168d0c17ff5ea9cc5d8c104300
Justin <jforseth210@gmail.com>
2021-02-17 14:24:51 -0700

    Cleanup resources.php (admin)

--- admin_templates/resources.php
+++ admin_templates/resources.php
@@ -8,10 +8,10 @@ if (isset($_POST['resourceNewSubmit'])) {
     $resourceBody = $_POST['resourceBody'];
 
     $resourceArray = array(
-      "resource_image_url" => $resourceImageURL,
-      "resource_name" => $resourceName,
-      "resource_url" => $resourceURL,
-      "resource_body" => $resourceBody
+        "resource_image_url" => $resourceImageURL,
+        "resource_name" => $resourceName,
+        "resource_url" => $resourceURL,
+        "resource_body" => $resourceBody
     );
     addNewRowJSON($resourceArray, "resources.json");
 }
@@ -26,10 +26,10 @@ if (isset($_POST['resourceUpdateSubmit'])) {
     $resourceBody = $_POST['resourceBody'];
 
     $resourceArray = array(
-      "resource_image_url" => $resourceImageURL,
-      "resource_name" => $resourceName,
-      "resource_url" => $resourceURL,
-      "resource_body" => $resourceBody
+        "resource_image_url" => $resourceImageURL,
+        "resource_name" => $resourceName,
+        "resource_url" => $resourceURL,
+        "resource_body" => $resourceBody
     );
 
     updateRowJSON($rowToUpdate, $resourceArray, "resources.json");
@@ -50,7 +50,7 @@ if (isset($_POST['resourceCardsReorderSubmit'])) {
     $old_index = intVal($_POST['old_index']);
     $new_index = $_POST['new_index'];
     reorderArrayJSON("resources.json", $old_index, $new_index);
-  }
+}
 ?>
 <section id="resources">
     <div class="container-fluid">
@@ -65,31 +65,31 @@ if (isset($_POST['resourceCardsReorderSubmit'])) {
                     for ($resource = 0; $resource <= sizeof($resourceArray) - 1; $resource++) {
                         $currentResource = $resourceArray[$resource];
                         //Create the start of the row, which is also a form.
-                        echo "
-                            <div class=\"col-md-4 d-flex\">
-                                <div class=\"card mx-auto w-100 my-5 d-flex zoom\">
-                                        <img class=\"card-img-top\" src=\"{$currentResource["resource_image_url"]}\">
-                                        <form role='form' id=\"resources$resource\" action='" . htmlspecialchars($_SERVER['PHP_SELF']) . "#resources" . "' method=\"POST\">
-                                        </form>
-                                            <h3><input form=\"resources$resource\" name=\"resourceImageURL\" class=\"erasable-value form-control\" value=\"{$currentResource["resource_image_url"]}\" placeholder=\"Image URL\"/></h3>
-                                            <h5><input form=\"resources$resource\" name=\"resourceName\" class=\"erasable-value form-control\" value=\"{$currentResource["resource_name"]}\" placeholder=\"Name\" /></h5>
-                                            <h5><input form=\"resources$resource\" name=\"resourceURL\" class=\"erasable-value form-control\" value=\"{$currentResource["resource_url"]}\" placeholder=\"Resource URL\" /></h5>
-                                            <textArea form=\"resources$resource\" style=\"min-height:100px !important\"name=\"resourceBody\" class=\"h-100 erasable-value form-control\" placeholder=\"Description\">" . $currentResource["resource_body"] . "</textarea>
-                                            <input hidden name=row_num form=\"resources$resource\" value=\"$resource\">
-                                            <input hidden name=row_num form=\"resources$resource" . "Delete\" value=\"$resource\">
+                    ?>
+                        <div class="col-md-4 d-flex">
+                            <div class="card mx-auto w-100 my-5 d-flex zoom">
+                                <img class="card-img-top" src="<?php echo $currentResource["resource_image_url"];?>">
+                                <form role='form' id="resources<?php echo $resource;?>" action='<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>#resources' method=" POST">
+                                </form>
+                                <h3><input form="resources<?php echo $resource;?>" name="resourceImageURL" class="erasable-value form-control" value="<?php echo $currentResource["resource_image_url"]; ?> " placeholder="Image URL" /></h3>
+                                <h5><input form="resources<?php echo $resource;?>" name="resourceName" class="erasable-value form-control" value="<?php echo $currentResource["resource_name"]; ?> " placeholder="Name" /></h5>
+                                <h5><input form="resources<?php echo $resource;?>" name="resourceURL" class="erasable-value form-control" value="<?php echo $currentResource["resource_url"]; ?> " placeholder="Resource URL" /></h5>
+                            <textArea form="resources<?php echo $resource;?>" style="min-height:100px !important"name="resourceBody" class="h-100 erasable-value form-control" placeholder="Description"><?php echo $currentResource["resource_body"];?></textarea>
+                                <input hidden name=row_num form="resources<?php echo $resource;?>" value="<?php echo $resource;?>">
+                                <input hidden name=row_num form="resources<?php echo $resource;?>Delete" value="<?php echo $resource;?>">
 
-                                            <form role='form' id=\"" . "resources" . $resource . "Delete\" action='" . htmlspecialchars($_SERVER['PHP_SELF']) . "#resources" . "' method=\" POST\">
-                                            </form>
-                                            <div role=\"group\" class=\"w-100 btn-group mx-auto mt-auto\">
-                                                <input form=\"" . "resources" . $resource . "Delete\" class=\"new-disable btn btn-danger mx-auto\" type=submit name=\"resourceDeleteSubmit\" value=\"Delete\" />
-                                                <input form=\"resources$resource\" class=\"btn btn-primary submit-button mx-auto\" type=submit name=\"resourceUpdateSubmit\" value=\"Save\" />
-                                                <button type=\"button\" class=\"new-disable btn btn-success mx-auto\" onclick=\"newRow('resourceCards',$resource);\">New</button>
-                                            </div>
-
+                                <form role='form' id="resources<?php echo $resource;?>Delete" action='<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>#resources' method=" POST">
+                                </form>
+                                <div role="group" class="w-100 btn-group mx-auto mt-auto">
+                                    <input form="resources<?php echo $resource;?>Delete" class="new-disable btn btn-danger mx-auto" type=submit name="resourceDeleteSubmit" value="Delete" />
+                                    <input form="resources<?php echo $resource;?>" class="btn btn-primary submit-button mx-auto" type=submit name="resourceUpdateSubmit" value="Save" />
+                                    <button type="button" class="new-disable btn btn-success mx-auto" onclick="newRow('resourceCards',<?php echo $resource;?>);">New</button>
                                 </div>
+
                             </div>
-                            ";
-                      };
+                        </div>
+                    <?php
+                    };
                     ?>
                 </div>
             </div>
