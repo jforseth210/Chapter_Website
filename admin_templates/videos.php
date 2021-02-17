8cbd16b8ddcfdc4b7c4882f36d028cae86798d8f
Justin <jforseth210@gmail.com>
2021-02-17 14:51:29 -0700

    Cleanup videos.php (admin)

--- admin_templates/videos.php
+++ admin_templates/videos.php
@@ -18,8 +18,8 @@ if (isset($_POST['videoNewSubmit'])) {
     //$videoInfo = str_replace("https://youtu.be/", "", $videoInfo);
 
     $videoArray = array(
-        "video_type"=>$videoType,
-        "video_url"=>$videoURL
+        "video_type" => $videoType,
+        "video_url" => $videoURL
     );
 
     addNewRowJSON($videoArray, "videos.json");
@@ -38,8 +38,8 @@ if (isset($_POST['videoUpdateSubmit'])) {
     //$videoURL = str_replace("https://", "", $video);
 
     $videoArray = array(
-        "video_type"=>$videoType,
-        "video_url"=>$videoURL
+        "video_type" => $videoType,
+        "video_url" => $videoURL
     );
 
     updateRowJSON($rowToUpdate, $videoArray, "videos.json");
@@ -92,47 +92,52 @@ if (isset($_POST['videoTableReorderSubmit'])) {
                             $videoTypesArray = array_keys($videoTypeConversion);
 
                             //Create the start of the row, which is also a form.
-                            echo "
-                                <tr>
-                                    <form role='form' id=\"videos$video\" action='" . htmlspecialchars($_SERVER['PHP_SELF']) . "#videos" . "' method=\"POST\">
-                                        <td>
-                                            <select form=\"videos$video\" class=\"form-control erasable-value\" name=\"videoType\">
-                            ";
+                        ?>
+                            <tr>
+                                <form role='form' id="videos<?php echo $video; ?>" action='<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>#videos' method="POST">
+                                    <td>
+                                        <select form="videos<?php echo $video; ?>" class="form-control erasable-value" name="videoType">
+                                            <?php
                                             for ($videoType = 0; $videoType <= sizeof($videoTypesArray) - 1; $videoType++) {
                                                 if ($videoTypeConversion[$videoTypesArray[$videoType]] == $currentVideoArray['video_type']) {
-                                                    echo "<option selected>{$videoTypesArray[$videoType]}</option>";
+                                            ?>
+                                                    <option selected><?php echo $videoTypesArray[$videoType]; ?></option>
+                                                <?php
                                                 } else {
-                                                    echo "<option>{$videoTypesArray[$videoType]}</option>";
+                                                ?>
+                                                    <option><?php echo $videoTypesArray[$videoType]; ?></option>
+                                            <?php
                                                 };
                                             };
-                            echo "
-                                            </select>
-                                        </td>
-                                    <td>
-                                        <input form=\"videos$video\" name=\"videoURL\" class=\"erasable-value form-control\" value=\"{$currentVideoArray['video_url']}\" />
+                                            ?>
+                                        </select>
                                     </td>
                                     <td>
-                                        <button type=\"button\" class=\"new-disable btn btn-success\" onclick=\"newRow('videoTable',$video);\">+</button>
-                                        <form role='form' id=\"" . "videos" . $video . "Delete\" action='" . htmlspecialchars($_SERVER['PHP_SELF']) . "#videos" . "' method=\"POST\">
-                                            <input class=\"new-disable btn btn-danger\" type=submit name=\"videoDeleteSubmit\" value=\"-\"/>
+                                        <input form="videos<?php echo $video; ?>" name="videoURL" class="erasable-value form-control" value="<?php echo $currentVideoArray['video_url']; ?>" />
+                                    </td>
+                                    <td>
+                                        <button type="button" class="new-disable btn btn-success" onclick="newRow('videoTable',<?php echo $video; ?>);">+</button>
+                                        <form role='form' id="videos" . <?php echo $video; ?>Delete" action='<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>#videos' method="POST">
+                                            <input class="new-disable btn btn-danger" type=submit name="videoDeleteSubmit" value="-" />
                                         </form>
                                     </td>
-                                    <td width=\"120px\">
-                                    <div class=\"btn btn-secondary handle\">
-                                <svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-arrows-move\" viewBox=\"0 0 16 16\">
-                                    <path fill-rule=\"evenodd\" d=\"M7.646.146a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 1.707V5.5a.5.5 0 0 1-1 0V1.707L6.354 2.854a.5.5 0 1 1-.708-.708l2-2zM8 10a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 0 1 .708-.708L7.5 14.293V10.5A.5.5 0 0 1 8 10zM.146 8.354a.5.5 0 0 1 0-.708l2-2a.5.5 0 1 1 .708.708L1.707 7.5H5.5a.5.5 0 0 1 0 1H1.707l1.147 1.146a.5.5 0 0 1-.708.708l-2-2zM10 8a.5.5 0 0 1 .5-.5h3.793l-1.147-1.146a.5.5 0 0 1 .708-.708l2 2a.5.5 0 0 1 0 .708l-2 2a.5.5 0 0 1-.708-.708L14.293 8.5H10.5A.5.5 0 0 1 10 8z\"/>
-                                </svg>
-                                </div>
+                                    <td width="120px">
+                                        <div class="btn btn-secondary handle">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrows-move" viewBox="0 0 16 16">
+                                                <path fill-rule="evenodd" d="M7.646.146a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 1.707V5.5a.5.5 0 0 1-1 0V1.707L6.354 2.854a.5.5 0 1 1-.708-.708l2-2zM8 10a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 0 1 .708-.708L7.5 14.293V10.5A.5.5 0 0 1 8 10zM.146 8.354a.5.5 0 0 1 0-.708l2-2a.5.5 0 1 1 .708.708L1.707 7.5H5.5a.5.5 0 0 1 0 1H1.707l1.147 1.146a.5.5 0 0 1-.708.708l-2-2zM10 8a.5.5 0 0 1 .5-.5h3.793l-1.147-1.146a.5.5 0 0 1 .708-.708l2 2a.5.5 0 0 1 0 .708l-2 2a.5.5 0 0 1-.708-.708L14.293 8.5H10.5A.5.5 0 0 1 10 8z" />
+                                            </svg>
+                                        </div>
                                     </td>
-                                    <input hidden name=row_num form=\"videos$video\" value=\"$video\">
-                                    <input hidden name=row_num form=\"videos$video" . "Delete\" value=\"$video\">
-                                    <input hidden name=row_num form=\"videos" . $video . "MoveUp\" value=\"$video\">
-                                    <input hidden name=row_num form=\"videos" . $video . "MoveDown\" value=\"$video\">
+                                    <input hidden name=row_num form="videos<?php echo $video; ?>" value="<?php echo $video; ?>">
+                                    <input hidden name=row_num form="videos<?php echo $video; ?>Delete" value="<?php echo $video; ?>">
+                                    <input hidden name=row_num form="videos" . <?php echo $video; ?>MoveUp" value="<?php echo $video; ?>">
+                                    <input hidden name=row_num form="videos" . <?php echo $video; ?>MoveDown" value="<?php echo $video; ?>">
                                     <td>
-                                        <input form=\"videos$video\" class=\"btn btn-primary submit-button\" type=submit name=\"videoUpdateSubmit\" value=\"Save\"/>
+                                        <input form="videos<?php echo $video; ?>" class="btn btn-primary submit-button" type=submit name="videoUpdateSubmit" value="Save" />
                                     </td>
                                 </form>
-                            </tr>";
+                            </tr>
+                        <?php
                         };
                         ?>
                     </tbody>
