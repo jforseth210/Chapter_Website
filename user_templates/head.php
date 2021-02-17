2f66beb0c37d68815a7e845931bf7c42b8057107
Justin <jforseth210@gmail.com>
2021-02-16 17:06:28 -0700

    Auto format head.php (user)

--- user_templates/head.php
+++ user_templates/head.php
@@ -1,79 +1,78 @@
 <head>
-  <!--Don't worry about any of this...-->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="Website for the Fairfield FFA Chapter.">
-  <meta name="author" content="Justin Forseth">
-  <!--The title at the top of the tab.-->
-  <title>Fairfield FFA</title>
+    <!--Don't worry about any of this...-->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Website for the Fairfield FFA Chapter.">
+    <meta name="author" content="Justin Forseth">
+    <!--The title at the top of the tab.-->
+    <title>Fairfield FFA</title>
 
-  <!-- Bootstrap core CSS -->
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-  <!-- Custom styles for this template -->
-  <link href="css/scrolling-nav.css" rel="stylesheet">
-  <link href="css/gallery.css" rel="stylesheet">
-  <!--Swiper CSS for photo gallery-->
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.4.0/css/swiper.min.css" rel="stylesheet">
-  <link rel="shortcut icon" href="images/emblem_favicon.png" type="image/x-icon">
-  <!--Cookie Notice-->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/Wruczek/Bootstrap-Cookie-Alert@gh-pages/cookiealert.css">
-  <style>
-    .youtube-player {
-        position: relative;
-        padding-bottom: 56.23%;
-        height: 0;
-        overflow: hidden;
-        max-width: 100%;
-        background: #000;
-        margin: 5px;
-    }
+    <!-- Custom styles for this template -->
+    <link href="css/scrolling-nav.css" rel="stylesheet">
+    <link href="css/gallery.css" rel="stylesheet">
+    <!--Swiper CSS for photo gallery-->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.4.0/css/swiper.min.css" rel="stylesheet">
+    <link rel="shortcut icon" href="images/emblem_favicon.png" type="image/x-icon">
+    <!--Cookie Notice-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/Wruczek/Bootstrap-Cookie-Alert@gh-pages/cookiealert.css">
+    <style>
+        .youtube-player {
+            position: relative;
+            padding-bottom: 56.23%;
+            height: 0;
+            overflow: hidden;
+            max-width: 100%;
+            background: #000;
+            margin: 5px;
+        }
 
-    .youtube-player iframe,
-    .youtube-player object,
-    .youtube-player embed {
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        z-index: 100;
-        background: transparent;
-    }
+        .youtube-player iframe,
+        .youtube-player object,
+        .youtube-player embed {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            z-index: 100;
+            background: transparent;
+        }
 
-    .youtube-player img {
-        bottom: 0;
-        display: block;
-        left: 0;
-        margin: auto;
-        max-width: 100%;
-        width: 100%;
-        position: absolute;
-        right: 0;
-        top: 0;
-        border: none;
-        height: auto;
-        cursor: pointer;
-        -webkit-transition: .4s all;
-        -moz-transition: .4s all;
-        transition: .4s all;
-    }
+        .youtube-player img {
+            bottom: 0;
+            display: block;
+            left: 0;
+            margin: auto;
+            max-width: 100%;
+            width: 100%;
+            position: absolute;
+            right: 0;
+            top: 0;
+            border: none;
+            height: auto;
+            cursor: pointer;
+            -webkit-transition: .4s all;
+            -moz-transition: .4s all;
+            transition: .4s all;
+        }
 
-    .youtube-player img:hover {
-        -webkit-filter: brightness(75%);
-    }
+        .youtube-player img:hover {
+            -webkit-filter: brightness(75%);
+        }
 
-    .youtube-player .play {
-        height: 72px;
-        width: 72px;
-        left: 50%;
-        top: 50%;
-        margin-left: -36px;
-        margin-top: -36px;
-        position: absolute;
-        background: url("//i.imgur.com/TxzC70f.png") no-repeat;
-        cursor: pointer;
-    }
-
-</style>
+        .youtube-player .play {
+            height: 72px;
+            width: 72px;
+            left: 50%;
+            top: 50%;
+            margin-left: -36px;
+            margin-top: -36px;
+            position: absolute;
+            background: url("//i.imgur.com/TxzC70f.png") no-repeat;
+            cursor: pointer;
+        }
+    </style>
 </head>
