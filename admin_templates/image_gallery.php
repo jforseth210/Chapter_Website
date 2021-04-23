5ea7bb30735d4598db5190e2cd0ed70628ab61de
Justin <jforseth210@gmail.com>
2021-04-23 10:15:23 -0600

    Move re-used functions to file_functions.php

--- admin_templates/image_gallery.php
+++ admin_templates/image_gallery.php
@@ -24,22 +24,6 @@ if (isset($_POST["photosNewSubmit"])) {
   echoToAlert($fileCount . " new photos added successfully");
 }
 
-// https://www.php.net/manual/en/features.file-upload.multiple.php
-function reArrayFiles($file_post) {
-
-  $file_ary = array();
-  $file_count = count($file_post['name']);
-  $file_keys = array_keys($file_post);
-
-  for ($i=0; $i<$file_count; $i++) {
-      foreach ($file_keys as $key) {
-          $file_ary[$i][$key] = $file_post[$key][$i];
-      }
-  }
-
-  return $file_ary;
-}
-
 //Replace an existing photo
 if (isset($_POST["photoSubmit"])) {
   //Read form data
