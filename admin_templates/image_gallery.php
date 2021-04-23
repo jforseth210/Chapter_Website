d2a661fdce8699912216b053e23e186d10df4055
Justin <jforseth210@gmail.com>
2021-04-23 09:50:33 -0600

    Add support for multiple photos

--- admin_templates/image_gallery.php
+++ admin_templates/image_gallery.php
@@ -2,30 +2,48 @@
 //Add a new photo
 if (isset($_POST["photosNewSubmit"])) {
   //Read the form data
-  $photo = $_FILES['photo'];
+  $photos = $_FILES['photos'];
+  $photos = reArrayFiles($photos);
+  
+  for ($i=0; $i < sizeof($photos); $i++) { 
+    //Figure out where to save it
+    $imageFileType = strtolower(pathinfo($photos[$i]["name"], PATHINFO_EXTENSION));
+    $target_dir = "images/gallery/";
+    $target_file = $target_dir . basename($photos[$i]["name"]);
+    //Create a new photo array for the 
+    //JSON file
+    $PhotoArray = array(
+      "path" => $target_file
+    );
+    
+    //Save it, update the JSON file,
+    //and tell the user
+    savePhoto($photos[$i], $target_file);
+    AddNewRowJSON($PhotoArray, "ImageGallery.json");
+  }
+  echoToAlert($fileCount . " new photos added successfully");
+}
 
-  //Figure out where to save it
-  $imageFileType = strtolower(pathinfo($photo["name"], PATHINFO_EXTENSION));
-  $target_dir = "images/gallery/";
-  $target_file = $target_dir . basename($photo["name"]);
+// https://www.php.net/manual/en/features.file-upload.multiple.php
+function reArrayFiles($file_post) {
 
-  //Create a new photo array for the 
-  //JSON file
-  $PhotoArray = array(
-    "path" => $target_file
-  );
+  $file_ary = array();
+  $file_count = count($file_post['name']);
+  $file_keys = array_keys($file_post);
 
-  //Save it, update the JSON file,
-  //and tell the user
-  savePhoto($photo, $target_file);
-  AddNewRowJSON($PhotoArray, "ImageGallery.json");
-  echoToAlert("Photo added successfully");
+  for ($i=0; $i<$file_count; $i++) {
+      foreach ($file_keys as $key) {
+          $file_ary[$i][$key] = $file_post[$key][$i];
+      }
+  }
+
+  return $file_ary;
 }
 
 //Replace an existing photo
 if (isset($_POST["photoSubmit"])) {
   //Read form data
-  $photo = $_FILES['photo'];
+  $photo = $_FILES['photo'][0];
   $rowToUpdate = intVal($_POST['row_num']);
 
   //Figure out where to save it 
@@ -69,7 +87,7 @@ if (isset($_POST['PhotoCardsReorderSubmit'])) {
     <div class="row">
       <div class="col-lg-12">
         <h2>Image Gallery</h2>
-        <b>Equally sized, landscape photos recommended, but not required.</b>
+        <b>Equally sized, landscape photos recommended, but not required. Uploading multiple photos is currently an experimental feature.</b>
         <div id="PhotoCards" class=row>
           <?php
           //Read and populate the original image data first.
@@ -85,7 +103,7 @@ if (isset($_POST['PhotoCardsReorderSubmit'])) {
                 <!-- File selection box -->
                 <form role='form' id="Photos<?php echo $Photo; ?>imagechange" action='<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>#photos' method="POST" enctype="multipart/form-data">
                   <div class="custom-file">
-                    <input type="file" class="new-load-file-function btn-file fresh-id form-control-file" onchange="loadFile(event, 'photosPhoto<?php echo $Photo; ?>' )" name="photo" id="customPhoto<?php echo $Photo; ?>">
+                    <input type="file" class="new-load-file-function btn-file fresh-id form-control-file" onchange="loadFile(event, 'photosPhoto<?php echo $Photo; ?>' )" name="photos[]" id="customPhoto<?php echo $Photo; ?>" multiple>
 
                     <label class="fresh-for custom-file-label" for="customPhoto<?php echo $Photo; ?>">Choose file</label>
                   </div>
